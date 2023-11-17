619a31bf6423f05ce7d74e6195fbe7fcea95b7ed
triplespamlegend <145274624+triplespamlegend@users.noreply.github.com>
2023-11-17 09:24:56 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -119,7 +119,6 @@ export default {
                     </p>
                 </div>
             </div>
-            <iframe src="https://discord.com/widget?id=1171445767647547414&theme=dark" width="350" height="500" allowtransparency="true" frameborder="0" sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"></iframe>
         </main>
     `,
     data: () => ({
