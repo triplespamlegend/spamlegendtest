56396dfe785a31d4acb16d2f239201d7e86aa399
triplespamlegend <145274624+triplespamlegend@users.noreply.github.com>
2024-11-11 01:35:28 -0600

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-41.5*Math.pow(rank-1, 0.51) + 200) *
+    let score = (-41.5*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
